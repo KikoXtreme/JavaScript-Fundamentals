0e1b3959ac6cb0f959706c1b626a0cda3fcc4d8a
Kiril Valkov <kiril_dgr@abv.bg>
2021-11-06 00:27:09 +0200

    Added smallestOfThreeNumbers.js

---
+++ 4-Functions/smallestOfThreeNumbers.js
@@ -0,0 +1,18 @@
+function smallestOfThreeNumbers(firstNum, secondNum, thirdNum) {
+    let smallestNum = Math.min(firstNum, secondNum, thirdNum);
+    return smallestNum;
+}
+
+console.log(smallestOfThreeNumbers(2, 5, 3));
+console.log(smallestOfThreeNumbers(600, 342, 123));
+console.log(smallestOfThreeNumbers(25, 21, 4));
+
+
+// function smallestOfThreeNumbers(firstNum, secondNum, thirdNum) {
+//     let smallestNum = Math.min(firstNum, secondNum, thirdNum);
+//     console.log(smallestNum);
+// }
+
+// smallestOfThreeNumbers(2, 5, 3);
+// smallestOfThreeNumbers(600, 342, 123);
+// smallestOfThreeNumbers(25, 21, 4);
