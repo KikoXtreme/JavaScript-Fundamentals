741cb1c812b7021b83e936abbc83d7357185e739
Kiril Valkov <kiril_dgr@abv.bg>
2021-12-06 16:16:49 +0200

    Added partyTime.js

---
+++ 7-Associative-Arrays/partyTime.js
@@ -0,0 +1,48 @@
+function partyTime(array) {
+    let inderOfPatry = array.indexOf('PARTY');
+    let digitList = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    let vip = [];
+    let regular = [];
+
+    for (let i = 0; i < inderOfPatry; i++) {
+        let currentGuest = array[i];
+        if (digitList.includes(currentGuest[0])) {
+            vip.push(currentGuest);
+        } else {
+            regular.push(currentGuest);
+        }
+    }
+
+    for (let i = inderOfPatry + 1; i < array.length; i++) {
+        let currentGuest = array[i];
+
+        if (vip.includes(currentGuest)) {
+            vip.splice(vip.indexOf(currentGuest), 1);
+        } else if (regular.includes(currentGuest)) {
+            regular.splice(regular.indexOf(currentGuest), 1);
+        }
+    }
+
+    let totalGuests = vip.length + regular.length;
+
+    console.log(totalGuests);
+
+    for (let guest of vip) {
+        console.log(guest);
+    }
+    for (let guest of regular) {
+        console.log(guest);
+    }
+}
+
+partyTime([
+    '7IK9Yo0h',
+    '9NoBUajQ',
+    'Ce8vwPmE',
+    'SVQXQCbc',
+    'tSzE5t0p',
+    'PARTY',
+    '9NoBUajQ',
+    'Ce8vwPmE',
+    'SVQXQCbc'
+]);
