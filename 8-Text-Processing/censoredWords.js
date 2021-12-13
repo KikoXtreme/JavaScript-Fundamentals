f0f7695bc5d4cb7c470439675e4c6417b262c4bd
Kiril Valkov <kiril_dgr@abv.bg>
2021-12-13 13:24:08 +0200

    Added censoredWords.js

---
+++ 8-Text-Processing/censoredWords.js
@@ -0,0 +1,6 @@
+function censoredWords(string, word) {
+    let result = string.split(word).join('*'.repeat(word.length));
+    console.log(result);
+}
+
+censoredWords("A small sentence with some words", "small");
